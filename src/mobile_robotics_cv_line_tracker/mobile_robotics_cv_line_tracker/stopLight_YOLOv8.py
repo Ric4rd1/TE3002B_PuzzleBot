5d06e402c4c94c9b1433a2b613df49e78793f799
Ricard Ubuntu <ric4rd11@gmail.com>
2025-06-02 17:35:36 -0600

    YOLOv8 inference node and best.py for traffic lights

---
+++ src/mobile_robotics_cv_line_tracker/mobile_robotics_cv_line_tracker/stopLight_YOLOv8.py
@@ -0,0 +1,66 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge
+from ultralytics import YOLO
+import numpy as np
+import cv2
+from rclpy.qos import qos_profile_sensor_data
+
+
+class YoloInference(Node):
+    def __init__(self):
+        super().__init__('yolo_node')
+        self.model = YOLO('/home/ricard/ros2_ws_puzzlebot/src/mobile_robotics_cv_line_tracker/models/best.pt')  # Load the YOLOv8 model
+        self.bridge = CvBridge()
+
+        self.sub = self.create_subscription(Image, 'video_source/raw', self.camera_callback, 10) # For Puzzlebot
+        self.pub = self.create_publisher(String, 'traffic_light', 10) 
+        self.yolo_img_pub =  self.create_publisher(Image, 'processed_img_traffic', qos_profile=qos_profile_sensor_data) 
+
+        self.img = None
+        timer_period = 1/20
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def camera_callback(self, msg):
+        try:
+            self.img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            cv2.GausssianBlur(self.img, (15, 15), 0, self.img)  # Apply Gaussian blur to the image
+            self.valid_img = True
+        except:
+            self.get_logger().info('Failed to get an image')
+
+    def timer_callback(self):
+        if self.img is None:
+            return
+
+        results = self.model(self.img)
+
+        # Annotate image
+        frame = results[0].plot()
+
+        # Publish annotated image
+        self.yolo_img_pub.publish(self.bridge.cv2_to_imgmsg(frame, encoding='bgr8'))
+
+        # Publish class name
+        for r in results:
+            for box in r.boxes:
+                class_id = int(box.cls)
+                class_name = self.model.names[class_id]
+                msg = String()
+                msg.data = class_name
+                self.pub.publish(msg)
+                break  # Only publish one class (first detection)
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    y_i = YoloInference()
+    rclpy.spin(y_i)
+    y_i.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
