ceccc8d96c5f644f729fb15d27f6de5dc0e765c4
Ricard Ubuntu <ric4rd11@gmail.com>
2025-06-06 10:32:37 -0600

    Added setup.py

---
+++ src/mobile_robotics_autonomous/setup.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'mobile_robotics_autonomous'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))), 
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ricard',
+    maintainer_email='ric4rd11@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'move_line_traffic = mobile_robotics_autonomous.move_line_traffic:main',
+            'move_points_traffic = mobile_robotics_autonomous.move_points_traffic:main',
+            'odom = mobile_robotics_autonomous.odom:main',
+            'streetSign_YOLOv8 = mobile_robotics_autonomous.streetSign_YOLOv8:main',
+            'stopLight_YOLOv8 = mobile_robotics_autonomous.stopLight_YOLOv8:main'
+        ],
+    },
+)
---
+++ src/mobile_robotics_closed_loop/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'mobile_robotics_closed_loop'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))), 
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.[yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ricard',
+    maintainer_email='ric4rd11@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'move_square = mobile_robotics_closed_loop.move_square:main',
+            'localization = mobile_robotics_closed_loop.localization:main',
+            'move_points = mobile_robotics_closed_loop.move_points:main',
+            'point_generator = mobile_robotics_closed_loop.point_generator:main',        ],
+    },
+)
